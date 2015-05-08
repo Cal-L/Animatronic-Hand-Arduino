28ecaf1972b66cec973f03971d2ee69ff2f68c2f
cleun007 <cleun007@gmail.com>
2015-05-07 19:19:34 -0700

    Initial Commit

---
+++ Flex_Sensor_Reciever/Flex_Sensor_Reciever.ino
@@ -0,0 +1,54 @@
+#include <Servo.h> 
+ 
+Servo myservo1;
+Servo myservo2;
+Servo myservo3;
+Servo myservo4;
+Servo myservo5;// create servo object to control a servo 
+                // a maximum of eight servo objects can be created 
+ 
+int pos = 0;    // variable to store the servo position
+int val1;
+int val2;
+int val3;
+int val4;
+int val5;
+ 
+void setup() 
+{ 
+  Serial.begin(9600);
+  myservo1.attach(3);
+  myservo2.attach(5);
+  myservo3.attach(9);
+  myservo4.attach(10);
+  myservo5.attach(11);  // attaches the servo on pin 9 to the servo object 
+} 
+ 
+ 
+void loop() 
+{ 
+if(Serial.available() >= 5){
+  val1 = Serial.read();
+  val2 = Serial.read();
+  val3 = Serial.read();
+  val4 = Serial.read();
+  val5 = Serial.read();
+  
+  Serial.print(val1);
+  Serial.print(" | ");
+  Serial.print(val2);
+  Serial.print(" | ");
+  Serial.print(val3);
+  Serial.print(" | ");
+  Serial.print(val4);
+  Serial.print(" | ");
+  Serial.println(val5);
+  
+  myservo1.write(val1*9);
+  myservo2.write(val2*9);
+  myservo3.write(val3*9);
+  myservo4.write(val4*9);
+  myservo5.write(val5*9);
+  delay(50);
+}
+}
---
+++ Flex_Sensor_Sender/Flex_Sensor_Sender.ino
@@ -0,0 +1,59 @@
+int const potPin1 = A0;
+int const potPin2 = A1;
+int const potPin3 = A2;
+int const potPin4 = A3;
+int const potPin5 = A4;
+int potVal1;
+int potVal2;
+int potVal3;
+int potVal4;
+int potVal5;
+
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  
+  potVal1 = analogRead(potPin1);
+  potVal1 = map(potVal1,515, 540,0,12);
+  potVal1 = constrain(potVal1,0,12);
+  Serial.write(potVal1);
+  
+  potVal2 = analogRead(potPin2);
+  potVal2 = map(potVal2,612, 670,0,12);
+  potVal2 = constrain(potVal2,0,12);
+  Serial.write(potVal2);
+  
+  potVal3 = analogRead(potPin3);
+  potVal3 = map(potVal3,570, 670,0,12);
+  potVal3 = constrain(potVal3,0,12);
+  Serial.write(potVal3);
+  
+  potVal4 = analogRead(potPin4);
+  potVal4 = map(potVal4,620, 680,0,12);
+  potVal4 = constrain(potVal4,0,12);
+  Serial.write(potVal4);
+  
+  potVal5 = analogRead(potPin5);
+  potVal5 = map(potVal5,510, 620,0,12);
+  potVal5 = constrain(potVal5,0,12);
+  Serial.write(potVal5);
+  
+  /*
+  Serial.print(" Pinky: ");
+  Serial.print(analogRead(A0));
+  Serial.print(" | Ring: ");
+  Serial.print(analogRead(A1));
+  Serial.print(" | Middle: ");
+  Serial.print(analogRead(A2));
+  Serial.print(" | Pointer: ");
+  Serial.print(analogRead(A3));
+  Serial.print(" | Thumb: ");
+  Serial.println(analogRead(A4));
+  */
+  
+  
+  delay(100);
+}
